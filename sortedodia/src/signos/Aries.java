db99d8727e9639cbf83236077945ce8887996583
Anderson Coimbra <andersonmegax@hotmail.com>
2015-03-19 02:56:27 -0300

    geração do javadoc com os devidos coomentarios em suas principais classes

---
+++ sortedodia/src/signos/Aries.java
@@ -0,0 +1,20 @@
+package signos;
+/**
+ * 
+ * @author Anderson Coimbra
+ *
+ */
+public class Aries extends Signos  {
+
+	public Aries(String nome) {
+		super(nome);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void frases() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- sortedodia/src/signos/Signos.java
+++ sortedodia/src/signos/Signos.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
+ *  Classe abstrata que sera usada para a construçao das subclasse signos 
  *
- * @author Usuario
+ * @author Anderson Coimbra
+ * 
  */
 public abstract class Signos {
     private String nome;
@@ -14,6 +16,18 @@ public abstract class Signos {
     private String fraseDoDia;
     private Random indice;
     
+    /**
+     *  Metodo construtor  usado para organizar e torna a subclase em um estado valido 
+     *  -> nome passado como paramentro sera o nome do usuario para se amazenado  
+     *  -> o metodo frase() sera responsavel por adiciona as lista de frases correspodente a cada signo no arraylist
+     *  -> a variavel indice sera do tipo Random que sera usado para selecionar a frase dentro do arraylist 
+     *  -> a variavel frasedodia sera do tipo String que concatenarar o nome do usuario mais a frase sorteada pelo indice
+     *  -> o metodo frase.get(int) responsavel por pegar um item do array é necessario um numero interio que corresponda o indece da frase desejada 
+     *  -> o metodo indice.nextInt(int) sera responsavel por gera um numero aleatorio que sera usado pelo metodo frase.get(), porem sera necessario fornecer um intervlo entre 0 e o numero maximo 
+     *  -> o metodo frase.size() retona o total de elemento no arraylist frase[], sera usado pelo indice.nextInt() para completar o processo para escolhe a frase aleatoria dentro do arraylist
+     *
+     * @param nome  nome passado pelo usuario  
+     */
     public Signos(String nome){
         this.nome = nome;
         frases();
@@ -22,9 +36,15 @@ public abstract class Signos {
     
     
     }
+    /**
+	 *  Esse metodo sera usado por subclasses de Signos para adiciona frases de sorte nos Arraylist frase
+	 */
     public abstract void frases();
     
-
+    /**
+     * Esse metodo ficara responsavel para fornecer a frase completa com o nome e a frase de sorte 
+     * @return
+     */
     public String getFraseDoDia() {
         return fraseDoDia;
     }
